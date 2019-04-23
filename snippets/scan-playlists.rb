81c95dd2baf440140236b3b892ce22a69ff35eed
Dmitry Sumskoy <dmitrysumskoy@MacBook-Pro-Dmitry.local>
2019-04-23 23:34:21 +0600

    Delete .rb file

--- snippets/scan-playlists.rb
+++
@@ -1,7 +0,0 @@
-def scan
-  @channel   = Channel.find_by(token: params[:token])
-  @playlists = @channel.channels_playlists.for_current_datetime.map(&:playlist)
-  @songs     = @playlists.collect(&:songs).flatten
-
-  render json: @songs, each_serializer: Cabinet::SongSerializer
-end
